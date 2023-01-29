0f9d8f2eb39f22c947711c328a3633573fc8c648
datanasov <dimiyat.g.atanasov@gmail.com>
2023-01-29 14:37:34 +0200

    Fix issues

--- webpack.config.dapp.js
+++ webpack.config.dapp.js
@@ -21,9 +21,7 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
+        type: 'asset'        
       },
       {
         test: /\.html$/,
